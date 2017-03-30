f9393dbc8f8b09d5b4f5ad27804bd55d5f95a3b2
sa.dev <sa.rg33@yahoo.fr>
2017-03-30 14:59:43 +0200

    delete config

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//connnect bdd
-$host  = 'localhost';
-$bdd   = 'chekpseudo';
-$user  = 'root';
-$password =  '';
-
-$db = new pdo('mysql:hostname='.$host.';dbname='.$bdd, $user, $password);
